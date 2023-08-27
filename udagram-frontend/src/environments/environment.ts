76d8f363b946d7c34652fab2779661d0eaa65ae4
linhlq_58 <lequyenlinh110@gmail.com>
2023-08-27 16:27:43 +0700

    Update config and environments

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a900b18e1961945a9a18e6f7c90e9bae-1216578404.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
